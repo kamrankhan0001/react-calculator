4f89b893649649533f9e2c4452d9547983088120
kamrankhan0001 <kamranbgp15@gmail.com>
2023-08-12 20:43:17 +0530

    react-calulator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import React,{useState} from "react";
+import "./App.css";
 
-function App() {
+
+function App(){
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [result, setResult] = useState('');
+    const [error, setError] = useState('');
+    
+  
+    const handleNum1Change = (event) => {
+      setNum1(event.target.value);
+    };
+  
+    const handleNum2Change = (event) => {
+      setNum2(event.target.value);
+    };
+  
+    const handleAddition = () => {
+      if (!validateInputs()) {
+        return;
+      }
+      const sum = parseFloat(num1) + parseFloat(num2);
+      setResult(sum);
+    };
+  
+    const handleSubtraction = () => {
+      if (!validateInputs()) {
+        return;
+      }
+      const difference = parseFloat(num1) - parseFloat(num2);
+      setResult(difference);
+    };
+  
+    const handleMultiplication = () => {
+      if (!validateInputs()) {
+        return;
+      }
+      const product = parseFloat(num1) * parseFloat(num2);
+      setResult(product);
+    };
+  
+    const handleDivision = () => {
+      if (!validateInputs()) {
+        return;
+      }
+      if (parseFloat(num2) === 0) {
+        setError('Error: Cannot divide by zero');
+        return;
+      }
+      const quotient = parseFloat(num1) / parseFloat(num2);
+      setResult(quotient);
+    };
+  
+    const validateInputs = () => {
+      if (!num1 || !num2) {
+        setError('Error: Num1 Can not be Empaty');
+        return false;
+      }
+      if (!/^-?\d*\.?\d+$/.test(num1) || !/^-?\d*\.?\d+$/.test(num2)) {
+        setError('Error: Please Enter Valid Numbers');
+        return false;
+      }
+      setError('');
+      return true;
+    };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Calculator</h1>
+      <div>
+        <input type="text" placeholder="Num1" value={num1} onChange={handleNum1Change} />
+        </div>
+      
+        <div>
+        <input type="text" placeholder="Num2" value={num2} onChange={handleNum2Change} />
+        </div>
+      <br />
+      
+      <div className="container1">
+      <button onClick={handleAddition}>+</button>
+      <button onClick={handleSubtraction}>-</button>
+      <button onClick={handleMultiplication}>*</button>
+      <button onClick={handleDivision}>/</button>
+      </div>
+      <br />
+      {error && <div style={{ color: '#FB184E' }}>{error}</div>}
+      {result && (
+        <div className="abc" style={{ color: '#0057FF' }}>
+          Result:{result}
+          
+          
+        </div>
+      )}
+      
     </div>
   );
 }
+
+    
 
 export default App;
